5ff94cbd2e519d010d9a90b5a2c9a19f329c9931
onokatio <onokatio@gmail.com>
2019-04-30 15:27:41 +0900

    form

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"crypto/sha256"
 )
 
 func main(){
@@ -23,9 +24,15 @@ func getIndex(c *gin.Context) {
 }
 
 func postIndex(c *gin.Context) {
-	c,PostForm("inputContent")
+	inputContent := c.PostForm("inputContent")
+	c.HTML(http.StatusOK, "complete.html", gin.H{
+		"inputContent" : inputContent,
+		"hash" : sha256.Sum256([]byte(inputContent)),
+		"publishTime" : 0,
+		"password" : "password",
+	})
 }
 
 func getContent(c *gin.Context) {
-	contentiD := c.Param("contentiD")
+	//contentiD := c.Param("contentiD")
 }
